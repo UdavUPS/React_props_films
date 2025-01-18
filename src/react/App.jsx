2598bbd678b31d5f8288784e7e79fe39e1b7d267
Udav-94 <ups-fti@yandex.ru>
2025-01-18 12:34:36 +0300

    Добавлены лэйблы с категориями, настроен подбор цветов

--- src/react/App.jsx
+++ src/react/App.jsx
@@ -10,8 +10,8 @@ function App() {
 
   return (
     <div className="body-box">
-      <FilmCart filmName={'Супермен'} bunerURL='https://i.pinimg.com/736x/10/f1/ec/10f1eccb0cce56738b7be42e3660da2a--superman-superhero.jpg' price={1299} count={4} liked={false}/>
-      <FilmCart filmName={'Одинокий странник'} bunerURL='https://i.pinimg.com/736x/91/40/c9/9140c95ace070d60bac78ee02833d3b7--cowboy-artwork-the-cowboy.jpg' price={899} count={2} liked={false}/>
+      <FilmCart filmName={'Супермен'} bunerURL='https://i.pinimg.com/736x/10/f1/ec/10f1eccb0cce56738b7be42e3660da2a--superman-superhero.jpg' price={1299} count={4} liked={false} category="экшн" labColor="action"/>
+      <FilmCart filmName={'Одинокий странник'} bunerURL='https://i.pinimg.com/736x/91/40/c9/9140c95ace070d60bac78ee02833d3b7--cowboy-artwork-the-cowboy.jpg' price={899} count={2} liked={false} category="вестерн" labColor="vestern"/>
     </div>
   );
 }
--- src/react/FilmCarts/FilmCarts.jsx
+++ src/react/FilmCarts/FilmCarts.jsx
@@ -4,16 +4,37 @@ import {Like} from '../Like/Like.jsx';
 import {Share} from '../Share/Share.jsx';
 
 
-export function FilmCart({filmName, bunerURL, price, count, liked }) {
+export function FilmCart({filmName, bunerURL, price, count, liked, category="фильм", labColor }) {
     let filmTitle: string = filmName;
     let buner: any = bunerURL;
     let cost: number = price;
+    const colorColection: any = {
+        action: "orange",
+        vestern: "brown"
+    };
+    let fonColor: any = colorColection[labColor];
+
+    
+    const styles = {
+        section: {
+            textTransform: "uppercase",
+            position: "relative",
+            backgroundColor: fonColor,
+            marginLeft: "175px",
+            bottom: "215px",
+            padding: "2.5% 10px",
+            color: "rgba(255, 255, 255, 0.792)",
+            boxShadow: "5px 5px 5px rgba(0, 0, 0, 0.26)",
+            display: "inline-block"
+        }
+      }
 
 
     return (
         <div  className="cart">
             <div className="cart__foto">
                 <img className="cart__foto__img" src={buner} alt="Постер фильма" />
+                <div className="cart__foto__movie-category" style={styles.section}>{category}</div>
             </div>
             <div className="cart__center">
                 <div className="cart__center__title">{filmTitle}</div>
